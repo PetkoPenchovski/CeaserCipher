cecf9ce0a0ccbb21e94d05342793b900750a7eec
Petko Penchovski <busec@abv.bg>
2018-02-10 17:30:16 +0200

    Create new method who find the most commot character and the distance between the most common chararter and the characters e,t,h. New documentation

--- src/CaeserCipher.java
+++ src/CaeserCipher.java
@@ -6,13 +6,17 @@ public class CaeserCipher {
         System.out.println("               \u0086 CAESER CIPHER \u0086");
         System.out.println("Insert encrypted text to see the decrypted results:");
         String text = input.nextLine();
+
         String otherOptions;
 
+        findingTheDistance(text);
 
         for (int i = 1; i <= 3; i++) {
+
             System.out.println(decrypt(text, i));
 
         }
+
         System.out.println("If you can`t find the word, write \"next\" to see the other options: ");
 
         otherOptions = input.nextLine();
@@ -23,6 +27,36 @@ public class CaeserCipher {
 
             }
 
+        }
+    }
+
+    public static void findingTheDistance(String text) {
+
+        int a[] = new int[26];
+        for (int i = 0; i < text.length(); i++) {
+            if (text.charAt(i) >= 'A' && text.charAt(i) <= 'Z') {
+                a[text.charAt(i) - 'A']++;
+
+            } else if (text.charAt(i) >= 'a' && text.charAt(i) <= 'z') {
+                a[text.charAt(i) - 'a']++;
+
+            }
+        }
+        int max = 0;
+        for (int i = 0; i < 26; i++) {
+            if (a[i] > a[max]) {
+                max = i;
+            }
+        }
+
+        char maxSymbol = (char) (max + 'a');
+        if (text.length() == 1 || a[max] > 1) {
+            System.out.println("'" + maxSymbol + "'" + " is: " + a[max] + " times in the text");
+            System.out.println("Distance to 'e': " + ('e' - maxSymbol));
+            System.out.println("Distance to 't': " + ('t' - maxSymbol));
+            System.out.println("Distance to 'h': " + ('h' - maxSymbol));
+        } else {
+            System.out.println("There is no most common character.");
         }
     }
 
